5dcb5d6ce0b2bf6d9377e05c9e11685893b39b2a
Kegan Maher <kegan.maher@gmail.com>
2023-11-06 00:13:30 -0800

    chore(main): log basic startup and completion message

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from app import wifi
+from app import logging, wifi
 from app.board import print_pins
 from app.util import get_env
+
+logger = logging.configure(__name__)
+
+logger.info("Main program starting")
 
 debug_mode = get_env("DEBUG", True, bool)
 if debug_mode:
@@ -13,3 +17,5 @@ if should_scan:
     wifi.scan()
 
 wifi.connect()
+
+logger.info("Main program complete")
